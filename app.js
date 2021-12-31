189b3b7b40ece09bc216922310069081d1e2b449
Ömer Can <67295748+omercandemir@users.noreply.github.com>
2022-01-01 00:28:11 +0300

    include io and http server.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = 5000;
 
 app.use("/", express.static(__dirname + '/public'));
 
-app.listen(port, () => {
+http.listen(port, () => {
     console.log("Server starting from: http://127.0.0.1:" + port);
 });
