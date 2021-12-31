046ac1ff39b659b29bfbd18e5697bafff19fb8fe
Ömer Can <67295748+omercandemir@users.noreply.github.com>
2022-01-01 01:03:45 +0300

    added throttle function and drawing functions.

--- public/script.js
+++ public/script.js
@@ -7,3 +7,66 @@ var drawing = false;
 var current = {
     color: "black"
 }
+
+function throttle(callback, delay) {
+    var previousCall = new Date().getTime();
+    return function() {
+        var time = new Date().getTime();
+        if (time - previousCall >= delay) {
+            previousCall = time;
+            callback.apply(null, arguments);
+        }
+    }
+}
+
+function drawLine(x0, y0, x1, y1, color, emit) {
+    context.beginPath();
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.strokeStyle = color;
+    context.stroke();
+    context.closePath();
+
+    if (!emit) {
+        return;
+    }
+    var w = canvas.width;
+    var h = canvas.height;
+
+    socket.emit("drawing", {
+        x0: x0 / w,
+        y0: y0 / h,
+        x1: x1 / w,
+        y1: y1 / h,
+        color
+    });
+}
+
+function maouseDown(e) {
+    drawing = true;
+    current.x = e.clientX || e.touches[0].clientX;
+    current.y = e.clientY || e.touches[0].clientY;
+}
+
+function maouseUp(e) {
+    if (!drawing) {
+        return;
+    }
+    drawing = false;
+}
+
+function maouseMove(e) {
+    if (!drawing) {
+        return;
+    }
+    drawLine(
+        currentX,
+        currentY,
+        e.clientX || e.touches[0].clientX,
+        e.clientY || e.touches[0].clientY,
+        current.color,
+        true
+    );
+    current.x = e.clientX || e.touches[0].clientX;
+    current.y = e.clientY || e.touches[0].clientY;
+}
